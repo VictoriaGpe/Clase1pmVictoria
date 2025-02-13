849b8803d300c68643d2a9e3c3b816439efd0da5
VictoriaGpe <vickyg3011@gmail.com>
2025-02-13 00:05:35 -0600

    Update problema12.py

---
+++ semana.3/problema12.py
@@ -0,0 +1,9 @@
+def mayor_de_tres(a, b, c):
+    return max(a, b, c)
+
+# Ejemplo de uso
+a = int(input("Ingresa el primer número: "))
+b = int(input("Ingresa el segundo número: "))
+c = int(input("Ingresa el tercer número: "))
+
+print(f"El mayor número es: {mayor_de_tres(a, b, c)}")
